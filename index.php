3b1599d5f5b69788b0f6883bc4c937b607a88ebd
germanosilva25 <42486346+germanosilva25@users.noreply.github.com>
2021-09-06 17:27:07 -0300

    Add files via upload
    Primeiro comiit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'model/Usuarios.php';
+
+$usuario = new Usuarios();
+
+//$usuario->connect();
+//$usuario->verifica();
+//phpInfo();exit;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Form control: input</h2>
+  <p>The form below contains two input elements; one of type text and one of type password:</p>
+  <form action="#">
+    <div class="form-group">
+      <label for="usr">Name:</label>
+      <input type="text" class="form-control" id="usr" name="username">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" name="password">
+    </div>
+    <button type="button" class="btn btn-primary">Enviar</button>
+  </form>
+</div>
+
+</body>
+</html>
